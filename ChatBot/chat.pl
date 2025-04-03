120befdfb9737d6cec07d1df00537e0ac4bfbb9d
Youra73 <houria.ariouat@student.unamur.be>
2025-04-03 12:28:38 +0200

    update chatbot + img lutin

--- ChatBot/chat.pl
+++ ChatBot/chat.pl
@@ -22,24 +22,87 @@ chat("Comment est-ce qu on joue à Pontu ?", Rules) :- rules(Rules).
 
 
 answer(Input, Answer) :-
-    findall(Similarity-Response, (
-        chat(Phrase, Response),
-        isub(Phrase, Input, Similarity, [true, true, 2])
-    ), Similarities),
+    findall(Similarity-Response, (chat(Phrase, Response),isub(Phrase, Input, Similarity, [true, true, 2])), Similarities),
     sort(Similarities, SortedSimilarities),
     reverse(SortedSimilarities, [BestSimilarity-BestResponse|_]),
-    (   BestSimilarity > 0.3
-    ->  Answer = BestResponse
-    ;   Answer = "Je ne sais pas"
-    ).
+    (BestSimilarity > 0.3->  Answer = BestResponse; Answer = "Je ne sais pas").
 
 chatbot :-
     repeat,
     write('Vous: '),
     read_line_to_string(user_input, Input),
-    (   Input = "Terminé"
-    ->  write('ChatJaiPété: Au revoir!'), nl, !
-    ;   answer(Input, Answer),
-        write('ChatJaiPété: '), write(Answer), nl,
-        fail
-    ).
+    (Input = "Terminé" ->  write('ChatJaiPété: Au revoir!'), nl, !; answer(Input, Answer), write('ChatJaiPété: '), write(Answer), nl,fail).
+
+
+/* --------------------------------------------------------------------- */
+/*                                                                       */
+/*             Transform questions into a list of words                  */
+/*                                                                       */
+/* --------------------------------------------------------------------- */
+% read_atomics(-ListOfAtomics)
+%  Reads a line of input, removes all punctuation characters, and converts
+%  it into a list of atomic terms, e.g., [this,is,an,example].
+
+read_atomics(ListOfAtomics) :-
+    read_lc_string(String),
+    clean_string(String,Cleanstring),
+    extract_atomics(Cleanstring,ListOfAtomics).
+
+
+ecrire_reponse(L) :-
+   nl, write('PBot :'),
+   ecrire_ligne(L,1,1,Mf).
+
+
+/* --------------------------------------------------------------------- */
+/*                                                                       */
+/*                         BOUCLE PRINCIPALE                             */
+/*                                                                       */
+/* --------------------------------------------------------------------- */
+
+pontuXL :- 
+
+   nl, nl, nl,
+   write('Bonjour, je suis PBot, le bot explicateur du jeu PontuXL.'), nl,
+   write('En quoi puis-je vous etre utile ?'), 
+   nl, nl, 
+
+   repeat,
+      write('Vous : '), ttyflush,
+      lire_question(L_Mots),
+      produire_reponse(L_Mots,L_reponse),
+      ecrire_reponse(L_reponse), nl,
+   fin(L_Mots), !.
+
+/* --------------------------------------------------------------------- */
+/*                                                                       */
+/*        PRODUIRE_REPONSE(L_Mots,L_strings) :                           */
+/*                                                                       */
+/*        Input : une liste de mots L_Mots representant la question      */
+/*                de l'utilisateur                                       */
+/*        Output : une liste de strings donnant la                       */
+/*                 reponse fournie par le bot                            */
+/*                                                                       */
+/*        NB Par défaut le predicat retourne dans tous les cas           */
+/*            [  "Je ne sais pas.", "Les étudiants",                     */
+/*               "vont m'aider, vous le verrez !" ]                      */
+/*                                                                       */
+/*        Je ne doute pas que ce sera le cas ! Et vous souhaite autant   */
+/*        d'amusement a coder le predicat que j'ai eu a ecrire           */
+/*        cet enonce et ce squelette de solution !                       */
+/*                                                                       */
+/* --------------------------------------------------------------------- */
+
+
+produire_reponse([fin],L1) :-
+    L1 = [merci, de, m, '\'', avoir, consulte], !.
+
+produire_reponse(L,Rep) :-
+    mclef(M,_), member(M,L),
+    clause(regle_rep(M,_,Pattern,Rep),Body),
+    match_pattern(Pattern,L), 
+    call(Body), !.
+
+produire_reponse(_,[S1,S2]) :-
+    S1 = "Je ne sais pas. ",
+    S2 = "Les étudiants vont m aider, vous le verrez".
