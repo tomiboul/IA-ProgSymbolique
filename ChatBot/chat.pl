ecc0db36984e5ec884974dbf5979ae9c74350d19
tomiboul <115427147+tomiboul@users.noreply.github.com>
2025-04-04 16:24:08 +0200

    ajout fichier manquant pour le code de jacquet

--- ChatBot/chat.pl
+++ ChatBot/chat.pl
@@ -31,7 +31,7 @@ chatbot :-
     repeat,
     write('Vous: '),
     read_line_to_string(user_input, Input),
-    (Input = "Terminé" ->  write('ChatJaiPété: Au revoir!'), nl, !; answer(Input, Answer), write('ChatJaiPété: '), write(Answer), nl,fail).
+    (Input = "Terminé" || "Au revoir" ->  write('ChatJaiPété: Au revoir!'), nl, !; answer(Input, Answer), write('ChatJaiPété: '), write(Answer), nl,fail).
 
 
 /* --------------------------------------------------------------------- */
@@ -50,8 +50,49 @@ read_atomics(ListOfAtomics) :-
 
 
 ecrire_reponse(L) :-
-   nl, write('PBot :'),
+   nl, 
+   write('PBot :'),
    ecrire_ligne(L,1,1,Mf).
+
+
+% ecrire_ligne(Li,Mi,Ei,Mf)
+% input : Li, liste de mots a ecrire
+%         Mi, indique si le premier caractere du premier mot 
+%            doit etre mis en majuscule (1 si oui, 0 si non)
+%         Ei, indique le nombre d'espaces avant ce premier mot 
+% output : Mf, booleen tel que decrit ci-dessus a appliquer 
+%          a la ligne suivante, si elle existe
+
+ecrire_ligne([],M,_,M) :- 
+    nl.
+ 
+ ecrire_ligne([M|L],Mi,Ei,Mf) :-
+    ecrire_mot(M,Mi,Maux,Ei,Eaux),
+    ecrire_ligne(L,Maux,Eaux,Mf).
+ 
+ % ecrire_mot(M,B1,B2,E1,E2)
+ % input : M, le mot a ecrire
+ %         B1, indique s'il faut une majuscule (1 si oui, 0 si non)
+ %         E1, indique s'il faut un espace avant le mot (1 si oui, 0 si non)
+ % output : B2, indique si le mot suivant prend une majuscule
+ %          E2, indique si le mot suivant doit etre precede d'un espace
+ 
+ ecrire_mot('.',_,1,_,1) :-
+    write('. '), !.
+ ecrire_mot('\'',X,X,_,0) :-
+    write('\''), !.
+ ecrire_mot(',',X,X,E,1) :-
+    espace(E), write(','), !.
+ ecrire_mot(M,0,0,E,1) :-
+    espace(E), write(M).
+ ecrire_mot(M,1,0,E,1) :-
+    name(M,[C|L]),
+    D is C - 32,
+    name(N,[D|L]),
+    espace(E), write(N).
+ 
+ espace(0).
+ espace(N) :- N>0, Nn is N-1, write(' '), espace(Nn).
 
 
 /* --------------------------------------------------------------------- */
@@ -61,7 +102,6 @@ ecrire_reponse(L) :-
 /* --------------------------------------------------------------------- */
 
 pontuXL :- 
-
    nl, nl, nl,
    write('Bonjour, je suis PBot, le bot explicateur du jeu PontuXL.'), nl,
    write('En quoi puis-je vous etre utile ?'), 
@@ -93,7 +133,6 @@ pontuXL :-
 /*                                                                       */
 /* --------------------------------------------------------------------- */
 
-
 produire_reponse([fin],L1) :-
     L1 = [merci, de, m, '\'', avoir, consulte], !.
 
@@ -103,6 +142,7 @@ produire_reponse(L,Rep) :-
     match_pattern(Pattern,L), 
     call(Body), !.
 
-produire_reponse(_,[S1,S2]) :-
+produire_reponse(_,[S1,S2,S3]) :-
     S1 = "Je ne sais pas. ",
-    S2 = "Les étudiants vont m aider, vous le verrez".
+    S2 = "Les étudiants vont m aider, vous le verrez",
+    S3 = "Je n'ai pas compris la question, est ce que vous pourriez la reformuler".
