09cb6039dbbb0f8f2e73dc153c3d5981ecac1fd1
tomiboul <115427147+tomiboul@users.noreply.github.com>
2025-04-05 19:04:47 +0200

    blqblqblqblebleveblublublu

--- chat.pl
+++ chat.pl
@@ -1,10 +1,10 @@
 :- use_module(library(isub)).
 :- use_module(library(readutil)).
 
-rules("Les règles du jeu Pontu sont les suivantes : ...").
+rules('Les règles du jeu Pontu sont les suivantes.').
 
 
-chat("Bonjour", "Bonjour, comment puis-je vous aider ?").
+chat("Bonjour", "Bonjour, comment puis je vous aider ?").
 chat("Salut", "Salut, quoi de neuf ?").
 chat("Wesh", "Wesh bien ou quoi ?").
 chat("Hola", "No habla español").
@@ -22,18 +22,19 @@ chat("Comment est-ce qu on joue à Pontu ?", Rules) :- rules(Rules).
 
 
 answer(Input, Answer) :-
-    findall(Similarity-Response, (chat(Phrase, Response), isub(Phrase, Input, Similarity, [true, true, 2])), Similarities),
-    sort(Similarities, SortedSimilarities),
-    reverse(SortedSimilarities, [BestSimilarity-BestResponse|_]),
-    (BestSimilarity > 0.3->  Answer = BestResponse; produire_reponse(_, X), random_member(X, Response), Answer = Response.
+   findall(Similarity-Response, (chat(Phrase, Response), isub(Phrase, Input, Similarity, [true, true, 2])), Similarities),
+   sort(Similarities, SortedSimilarities),
+   reverse(SortedSimilarities, [BestSimilarity-BestResponse|_]),
+   (BestSimilarity > 0.3->  Answer = BestResponse; Answer = 'Je ne sais pas').
+
 
 chatbot :-
-    repeat,
-    write('Vous: '),
-    read_line_to_string(user_input, Input),
-    (Input = "Terminé" || "Au revoir" ->  write('ChatJaiPété: Au revoir!'), nl, !; answer(Input, Answer), write('ChatJaiPété: '), write(Answer), nl,fail).
-
-
+   repeat,
+   write('Vous: '),
+   read_line_to_string(user_input, Input),
+   (Input = "Terminé" || "Au revoir" ->  write('ChatJaiPété: Au revoir!'), nl, !; answer(Input, Answer), write('ChatJaiPété: '), write(Answer), nl,fail).
+   
+   
 /* --------------------------------------------------------------------- */
 /*                                                                       */
 /*             Transform questions into a list of words                  */
@@ -44,15 +45,15 @@ chatbot :-
 %  it into a list of atomic terms, e.g., [this,is,an,example].
 
 read_atomics(ListOfAtomics) :-
-    read_lc_string(String),
-    clean_string(String,Cleanstring),
-    extract_atomics(Cleanstring,ListOfAtomics).
+   read_lc_string(String),
+   clean_string(String,Cleanstring),
+   extract_atomics(Cleanstring,ListOfAtomics).
 
 
 ecrire_reponse(L) :-
-   nl, 
-   write('PBot :'),
-   ecrire_ligne(L,1,1,Mf).
+  nl, 
+  write('PBot :'),
+  ecrire_ligne(L,1,1,Mf).
 
 
 % ecrire_ligne(Li,Mi,Ei,Mf)
@@ -64,35 +65,35 @@ ecrire_reponse(L) :-
 % a la ligne suivante, si elle existe
 
 ecrire_ligne([],M,_,M) :- 
-    nl.
- 
- ecrire_ligne([M|L],Mi,Ei,Mf) :-
-    ecrire_mot(M,Mi,Maux,Ei,Eaux),
-    ecrire_ligne(L,Maux,Eaux,Mf).
- 
- % ecrire_mot(M,B1,B2,E1,E2)
- % input : M, le mot a ecrire
- %         B1, indique s il faut une majuscule (1 si oui, 0 si non)
- %         E1, indique s il faut un espace avant le mot (1 si oui, 0 si non)
- % output : B2, indique si le mot suivant prend une majuscule
- %          E2, indique si le mot suivant doit etre precede d'un espace
- 
- ecrire_mot('.',_,1,_,1) :-
-    write('. '), !.
- ecrire_mot('\'',X,X,_,0) :-
-    write('\''), !.
- ecrire_mot(',',X,X,E,1) :-
-    espace(E), write(','), !.
- ecrire_mot(M,0,0,E,1) :-
-    espace(E), write(M).
- ecrire_mot(M,1,0,E,1) :-
-    name(M,[C|L]),
-    D is C - 32,
-    name(N,[D|L]),
-    espace(E), write(N).
- 
- espace(0).
- espace(N) :- N>0, Nn is N-1, write(' '), espace(Nn).
+   nl.
+
+ecrire_ligne([M|L],Mi,Ei,Mf) :-
+   ecrire_mot(M,Mi,Maux,Ei,Eaux),
+   ecrire_ligne(L,Maux,Eaux,Mf).
+
+% ecrire_mot(M,B1,B2,E1,E2)
+% input : M, le mot a ecrire
+%         B1, indique s il faut une majuscule (1 si oui, 0 si non)
+%         E1, indique s il faut un espace avant le mot (1 si oui, 0 si non)
+% output : B2, indique si le mot suivant prend une majuscule
+%          E2, indique si le mot suivant doit etre precede d'un espace
+
+ecrire_mot('.',_,1,_,1) :-
+   write('. '), !.
+ecrire_mot('\'',X,X,_,0) :-
+   write('\''), !.
+ecrire_mot(',',X,X,E,1) :-
+   espace(E), write(','), !.
+ecrire_mot(M,0,0,E,1) :-
+   espace(E), write(M).
+ecrire_mot(M,1,0,E,1) :-
+   name(M,[C|L]),
+   D is C - 32,
+   name(N,[D|L]),
+   espace(E), write(N).
+
+espace(0).
+espace(N) :- N>0, Nn is N-1, write(' '), espace(Nn).
 
 
 /* --------------------------------------------------------------------- */
@@ -106,14 +107,14 @@ pontuXL :-
    write('Bonjour, je suis PBot, le bot explicateur du jeu PontuXL.'), nl,
    write('En quoi puis-je vous etre utile ?'), 
    nl, nl, 
-
+ 
    repeat,
       write('Vous : '), ttyflush,
       lire_question(L_Mots),
       produire_reponse(L_Mots,L_reponse),
       ecrire_reponse(L_reponse), nl,
    fin(L_Mots), !.
-
+ 
 /* --------------------------------------------------------------------- */
 /*                                                                       */
 /*        PRODUIRE_REPONSE(L_Mots,L_strings) :                           */
@@ -134,19 +135,18 @@ pontuXL :-
 /* --------------------------------------------------------------------- */
 
 produire_reponse([fin],L1) :-
-    L1 = [merci, de, m, '\'', avoir, consulte], !.
+   L1 = [merci, de, m, '\'', avoir, consulte], !.
 
 produire_reponse(L,Rep) :-
-    mclef(M,_), member(M,L),
-    clause(regle_rep(M,_,Pattern,Rep),Body),
-    match_pattern(Pattern,L), 
-    call(Body), !.
+   mclef(M,_), member(M,L),
+   clause(regle_rep(M,_,Pattern,Rep),Body),
+   match_pattern(Pattern,L), 
+   call(Body), !.
 
 produire_reponse(_,[S1,S2,S3,S4]) :-
-    S1 = "Je ne sais pas, je n ai actuellement pas encore la réponse.",
-    S2 = "Les étudiants vont m aider, vous le verrez",
-    S3 = "Je n ai pas compris la question, est ce que vous pourriez la reformuler ? ",
-    S4 = "Je ne suis pas sur de connaitre la réponse à votre question, peut etre pourriez vous la reformuler ? ".
+   S1 = "Je ne sais pas, je n ai actuellement pas encore la réponse.",
+   S2 = "Les étudiants vont m aider, vous le verrez.",
+   S3 = "Je n ai pas compris la question, est ce que vous pourriez la reformuler ? ".
 
 
 
@@ -189,8 +189,9 @@ Dist = min (lev(a-1, b)
 
 
 lev_list([_ | T1], [_ | T2], Dist) :-
-   lev_list(T1, [_ | T2], Dist1),  % Opération n°1: suppression dans A
-   lev_list([_ | T1], T2, Dist2),  % Opération n°2: suppression dans B
-   lev_list(T1, T2, Dist3),        % Opération n°3: remplacement
+   lev_list(T1, [_ | T2], Dist1),  
+   lev_list([_ | T1], T2, Dist2),  
+   lev_list(T1, T2, Dist3),        
    min3(Dist1, Dist2, Dist3, Min),
-   Dist is Min + 1.                % Tenir compte de l'opération effectuée
+   Dist is Min + 1.
+
