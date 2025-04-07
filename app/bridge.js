7c57511bf03a12bddbd189b03c240bf5c9338132
ilias0prog <123807317+ilias0prog@users.noreply.github.com>
2025-04-07 18:52:53 +0200

    Board update

--- app/bridge.js
+++ app/bridge.js
@@ -14,26 +14,11 @@ for (let col = 0; col < totalSize; col++) {
 
         else if (col % 2 === 0 && row % 2 === 1) {
             div.className = "h-bridge-container";
-            const hBridge = document.createElement("img");
 
-            hBridge.src = "images/bridge.png";
-            hBridge.alt = "Pont vertical";
-
-            hBridge.className = "h-bridge"
-
-            // div.append(hBridge);
         }
 
         else if (col % 2 === 1 && row % 2 === 0) {
             div.className = "v-bridge-container";
-            const hBridge = document.createElement("img");
-
-            hBridge.src = "images/bridge.png";
-            hBridge.alt = "Pont horizontal";
-
-            hBridge.className = "v-bridge"
-
-            // div.append(hBridge);
         }
 
         else {
