867395e97c7bddcd85fb8c613b8c65bf94067f69
Youra73 <houria.ariouat@student.unamur.be>
2025-04-09 15:47:43 +0200

    amelioration spawn

--- app/js/game.js
+++ app/js/game.js
@@ -1,8 +1,7 @@
 //starting the game
 let startgame = false
 let tour = 0
-let vert = true
-
+let matrix = [];
 
 const start = document.getElementById("startgame");
 
@@ -11,15 +10,44 @@ start.addEventListener("click", function(){
     console.log(tour);
     console.log("ok");
     tour = 0;
+    free = {};
     const images = document.querySelectorAll('img'); 
     images.forEach(image => {
         image.style.display = 'none';
     });
+    //matrice a changer car represente pas les ponts
+    matrix = 
+    [[false, false, false, false, false, false],
+    [false, false, false, false, false, false],
+    [false, false, false, false, false, false],
+    [false, false, false, false, false, false],
+    [false, false, false, false, false, false],
+    [false, false, false, false, false, false]];
+
   });
+//function to retrieve the position of an elf 
+function getData(form) {
+    var formData = new FormData(form);
+  
+    for (var pair of formData.entries()) {
+      console.log(pair[0] + ": " + pair[1]);
+    }
+    
+    console.log(Object.fromEntries(formData));
+    const data = Object.fromEntries(formData)
+
+    const position = data.position.split('-');
+    const x = position[0];
+    const y = position[1];
+    position_x_y = {x, y};
+    return position_x_y;
+  }
+  
+  
+
 
 //function to spawn an elf
-
-function spawn () {
+function spawn (position) {
     if (startgame === true) {
         let couleur = "";
         if (tour % 4 === 0) couleur = "vert";
@@ -31,14 +59,35 @@ function spawn () {
         const lutin = document.createElement("img");
         lutin.src = `/app/images/lutin_${couleur}.png`; 
         lutin.alt = `Lutin ${couleur}`;
-        lutin.style.width = "100px";
-        lutin.style.margin = "10px";
-    
+        lutin.className = "lutin";
 
-        const container = document.getElementById("lutin-container");
-        container.appendChild(lutin);
+        const x = position.x; 
+        const y = position.y;
+      
+
+
+        if (matrix[x-1][y-1] == false) {
+          const cell = document.getElementById(`${position.x}-${position.y}`);
+          if (cell) {
+            cell.append(lutin);
+            matrix[x-1][y-1] = true;
+            tour ++;
+          }
+          console.log(matrix);
+        
+        }
+
         console.log(tour);
-        tour ++;
+        
     }
 }
 
+
+
+document.getElementById("position").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const position = getData(e.target);
+    console.log("voila la position avant d'etre dans fct :", position);
+    spawn (position);
+
+  });
