e27b8250768305480e5b86fe79cf704fbe643480
tomiboul <115427147+tomiboul@users.noreply.github.com>
2025-04-17 15:08:54 +0200

    version deplcament focntionnel

--- jeu.pl
+++ jeu.pl
@@ -1,4 +1,4 @@
-:- module(jeu, [ajoutLutin/3]).
+%% :- module(jeu, [ajoutLutin/3]).
 
 
 /*################## Les couleurs ####################*/
@@ -79,6 +79,22 @@ deplacePont(((X1, Y1)-(X2,Y2)), ((NewX1, NewY1)-(NewX2,NewY2)), ListePont, NewLi
     Y2 =< 6,
     Y2 >= 1,
 
+
+    member(((X1, Y1)-(X2,Y2)), ListePont),
+    
+
+    %% Rotation de 90° du pont horizontal vers vertical (avec du coup les 4 possibilités)
+    (Y1 = Y2 ->((NewX1 = X1, NewY1 is Y1 - 1, NewX2 = X1, NewY2 = Y1) ; 
+                (NewX1 = X1, NewY1 = Y1, NewX2 = X1, NewY2 is Y1 + 1) ; 
+                (NewX1 = X2, NewY1 is Y1 - 1, NewX2 = X2, NewY2 = Y1);
+                (NewX1 = X2, NewY1 = Y1, NewX2 = X2, NewY2 is Y1 + 1))
+    ; %% Rotation de 90° du pont vertical vers horizontal (avec du coup les 4 possibilités)
+        ((NewX1 is X1 - 1, NewY1 = Y1, NewX2 = X1, NewY2 is Y1);
+        (NewX1 = X1, NewY1 = Y1, NewX2 is X1 + 1, NewY2 is Y1);
+        (NewX1 is X1 - 1, NewY1 = Y2, NewX2 = X1, NewY2 is Y2);            
+        (NewX1 = X1, NewY1 = Y2, NewX2 is X1 + 1, NewY2 is Y2))
+    ),
+    
     NewX1 =< 6,
     NewX1 >= 1,
     NewY1 =< 6,
@@ -87,29 +103,10 @@ deplacePont(((X1, Y1)-(X2,Y2)), ((NewX1, NewY1)-(NewX2,NewY2)), ListePont, NewLi
     NewX2 >= 1,
     NewY2 =< 6,
     NewY2 >= 1,
-    member(((X1, Y1)-(X2,Y2)), ListePont),
-    
-    /*( %% On vérifie que le déplacement demander soit correcte ( rotation de 90° la case) 
-     (X1 = NewX1, Y1 = NewY1, X2 = NewX2, (Y2 is NewY2 + 1 ; Y2 is NewY2 - 1)) ;
-     (X1 = NewX1, Y1 = NewY1, (X2 is NewX2 + 1 ;X2 is NewX2 - 1 ), Y2 = NewY2) ;
-     (X1 = NewX1, (Y1 is NewY1 + 1 ; Y1 is NewY1 - 1), X2 = NewX2, Y2 = NewY2) ;
-     ((X1 is NewX1 + 1 ; X1 is NewX1 - 1), Y1 = NewY1, X2 = NewX2,  Y2 = NewY2) 
-    ),*/
 
-    (( /* Rotation autour de X1,Y1*/
-        (X1 = NewX1, Y1 = NewY1), ((X2 is NewX2 - 1), (Y2 is NewY2 -1 ;  Y2 is NewY2 +1));
-        (X1 = NewX1, Y1 = NewY1), ((Y2 is NewY2 - 1), (X2 is NewX2 -1 ;  X2 is NewX2 +1));
-        (X1 = NewX1, Y1 = NewY1), ((Y2 is NewY2 + 1), (X2 is NewX2 -1 ;  X2 is NewX2 +1));
-        (X1 = NewX1, Y1 = NewY1), ((X2 is NewX2 + 1), (Y2 is NewY2 -1 ;  Y2 is NewY2 +1))
-    );( /* Rotation autour de X2,Y2*/
-        (X2 = NewX2, Y2 = NewY2), ((X1 is NewX1 - 1), (Y1 is NewY1 -1 ;  Y1 is NewY1 +1));
-        (X2 = NewX2, Y2 = NewY2), ((Y1 is NewY1 - 1), (X1 is NewX1 -1 ;  X1 is NewX1 +1));
-        (X2 = NewX2, Y2 = NewY2), ((Y1 is NewY1 + 1), (X1 is NewX1 -1 ;  X1 is NewX1 +1));
-        (X2 = NewX2, Y2 = NewY2), ((X1 is NewX1 + 1), (Y1 is NewY1 -1 ;  Y1 is NewY1 +1))
-    )),
     not(member(((NewX1, NewY1)-(NewX2,NewY2)), ListePont)), %% on va vérifier qu il n existe pas deja de pont a cette position
     suppPont(((X1, Y1)-(X2,Y2)), ListePont, TempNewListePont),
-    NewListePont = [((NewX1, NewY1)-(NewX2,NewY2))|TempNewListePont].
+    NewListePont = [((NewX1, NewY1)-(NewX2,NewY2))|TempNewListePont], write("\n").
     
 
 
