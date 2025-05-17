6ce6286deb8ffe5f63db339380845bf850b19fdf
tomiboul <115427147+tomiboul@users.noreply.github.com>
2025-05-17 13:40:55 +0200

    push placment 1erversion

---
+++ placementIA.pl
@@ -0,0 +1,79 @@
+:- module(placementIA, []).
+:- use_module(situation, [rotation/2]).
+
+/*
+listes des ponts 
+[(1,1)-(1,2), (1,2)-(1,3), (1,3)-(1,4), (1,4)-(1,5), (1,5)-(1,6), (2,1)-(2,2), (2,2)-(2,3), (2,3)-(2,4), (2,4)-(2,5), (2,5)-(2,6), (3,1)-(3,2), (3,2)-(3,3), (3,3)-(3,4), (3,4)-(3,5), (3,5)-(3,6), (4,1)-(4,2), (4,2)-(4,3), (4,3)-(4,4), (4,4)-(4,5), (4,5)-(4,6), (5,1)-(5,2), (5,2)-(5,3), (5,3)-(5,4), (5,4)-(5,5), (5,5)-(5,6), (6,1)-(6,2), (6,2)-(6,3), (6,3)-(6,4), (6,4)-(6,5), (6,5)-(6,6), (1,1)-(2,1), (2,1)-(3,1), (3,1)-(4,1), (4,1)-(5,1), (5,1)-(6,1), (1,2)-(2,2), (2,2)-(3,2), (3,2)-(4,2), (4,2)-(5,2), (5,2)-(6,2), (1,3)-(2,3), (2,3)-(3,3), (3,3)-(4,3), (4,3)-(5,3), (5,3)-(6,3), (1,4)-(2,4), (2,4)-(3,4), (3,4)-(4,4), (4,4)-(5,4), (5,4)-(6,4), (1,5)-(2,5), (2,5)-(3,5), (3,5)-(4,5), (4,5)-(5,5), (5,5)-(6,5), (1,6)-(2,6), (2,6)-(3,6), (3,6)-(4,6), (4,6)-(5,6), (5,6)-(6,6)]
+*/
+
+/**
+ * (IN, OUT)
+ * Crée un nouveau lutin sur une base aléatoire
+*/
+/*placementLutinHeuristique1((ListeLutin, ListePont, Ordrejeu), (NextListeLutin, ListePont, NextOrdreJeu)):-
+    coordonneeNewLutin((ListeLutin, _, Ordrejeu), NewLutin),
+    not(member(NewLutin, ListeLutin)),
+    NextListeLutin is [NewLutin|ListeLutin],
+    rotation(OrdreJeu, NextOrdreJeu), !.*/
+
+/**
+ * (IN, OUT)
+ * Crée un nouveau lutin sur une base aléatoire
+*/
+placementLutinHeuristique2((ListeLutin, ListePont, [Couleur|Restant]), (NextListeLutin, ListePont, NextOrdreJeu)):-
+    random_between(1, 6, NewX),
+    random_between(1, 6, NewY),
+    not(member((Couleur, NewX, NewY), ListeLutin)),
+    NextListeLutin = [(Couleur, NewX, NewY)|ListeLutin],
+    rotation([Couleur|Restant], NextOrdreJeu), !.
+         
+
+%coordonneeNewLutin((ListeLutin, ListePont, Ordrejeu), NewLutin):-
+
+/**
+ * (IN,OUT)
+ * génère la liste des positions possible du prochain lutin (en enlevant les positions des lutins déja présents)
+*/
+genereListePosition(ListeLutin, Result):-
+    genereListePositionAcc(1,1,[], TempResult), 
+    findall((X,Y), 
+            (member((X,Y), TempResult), not(member((_,X,Y), ListeLutin))), 
+            Result),
+    !.
+genereListePositionAcc(6,6, Acc, [(6,6)|Acc]):- !.
+genereListePositionAcc(X,Y, Acc, Result):-
+    (Y < 6 -> NewY is Y + 1, genereListePositionAcc(X, NewY, [(X,Y)|Acc], Result)
+    ;  NewX is X + 1,  genereListePositionAcc(NewX, 1, [(X,Y)|Acc], Result)
+    ), !.
+
+
+/**
+ * (IN, IN, OUT)
+ * Donne la liste des distances d'une position potentiel du nouveau lutin avec les lutins déja existants
+*/
+distanceManhattan(PositionPotentiel, ListeLutin, ListeDistant):-
+    distanceManhattanAcc(PositionPotentiel, ListeLutin, [], ListeDistant), !.
+distanceManhattanAcc(_, [], Acc, Acc):- !.
+distanceManhattanAcc((NextX, NextY), [(_, X, Y)|Restant], Acc, ListeDistant):-
+    Distance is  abs(NextX - X) + abs(NextY - Y),
+    distanceManhattanAcc((NextX, NextY), Restant, [(Distance, X, Y)|Acc], ListeDistant).
+    
+
+/**
+ * (IN, OUT)
+ * Calcul la distance minimal parmis une liste de distance
+*/
+distanceMin(ListeDistant, Result):-
+    distanceMinAcc(ListeDistant, (10000000, 10000000, 10000000), Result).
+distanceMinAcc([], Acc, Acc).
+distanceMinAcc([(Distance, X,Y)|Reste], (DistanceAcc, XAcc, YAcc), Result):-
+    (Distance < DistanceAcc 
+        -> NewDistanceAcc is Distance, 
+            NewXAcc is X, 
+            NewYAcc is Y, 
+            distanceMinAcc(Reste, (NewDistanceAcc, NewXAcc, NewYAcc), Result)
+        ;
+        distanceMinAcc(Reste, (DistanceAcc, XAcc, YAcc), Result)).
+
+
+%genereListeDistanceMin(ListePositionPotentiel, )
